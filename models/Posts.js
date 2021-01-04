bb0af47905b8c0a36277c6648e172220d826a877
davi <davi.p.m94@gmail.com>
2021-01-04 02:35:48 -0300

    adding post model

---
+++ models/Posts.js
@@ -0,0 +1,18 @@
+import { Schema, model } from "mongoose";
+
+const PostSchema = new Schema({
+  title: String,
+  message: String,
+  creator: String,
+  tags: [String],
+  likeCount: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+});
+
+export default model("PostMessage", PostSchema);
